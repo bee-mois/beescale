7dd9963cc5fe8f2055edf17853297352ba7ecb8a
bee-mois <bee-mois@users.noreply.github.com>
2017-03-28 22:28:36 +0200

    Create latest.sh

---
+++ latest.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+cd /home/www/wbk/euse.de/honig/beescale
+wget wget.log http://wbk.in-vpn.de/sd/[path to webcam folder]/latest.jpg -O latest_new.jpg
+if [ cmp -s latest.jpg latest_new.jpg ] ; then
+        rm latest_new.jpg
+else
+		date=$(stat latest_new.jpg | grep Modify | cut -d ' ' -f 2,3 | cut -d ':' -f1,2)
+		convert latest_new.jpg -gravity SouthEast -pointsize 11 -fill white -annotate +5+5 "$date" latest.jpg
+      	rm latest_new.jpg
+fi
+exit 0
